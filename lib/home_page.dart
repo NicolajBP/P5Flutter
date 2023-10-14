a8c9214d027d82df028bdf26af78ba0909dc2927
Hamada-dada <147512372+Hamada-dada@users.noreply.github.com>
2023-10-14 15:39:01 +0200

    logout button and a bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:p5/report_exercise.dart';
 import 'package:p5/report_nutrient_intake.dart';
@@ -5,57 +6,74 @@ import 'package:p5/test_page.dart'; // Vi skal altid have den her import (giver 
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+//sign user out 
+void signUserOut(){
+  FirebaseAuth.instance.signOut();
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-              color: Colors.lightBlue,
-              margin: const EdgeInsets.fromLTRB(100, 20, 100, 20),
-              padding: const EdgeInsets.all(50),
-              height: 250,
-              width: double.infinity,
-              child: const Text("Her kommer der en graf"),
-            ),
-      const SizedBox(height: 5),
-      Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              // Bruges som "router" --> vi føres fra denne side til en anden side (i det her tilfælde vores ReportNutrientIntakePage)
-              return const ReportNutrientIntakePage();
-            }));
-          },
-          child: const Text("Report nutrient intake"),
+
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      actions: [
+        IconButton(
+          onPressed: signUserOut,
+          icon: Icon(Icons.logout),
         ),
-      ),
-      const SizedBox(height: 5),
-      Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              // Bruges som "router" --> vi føres fra denne side til en anden side (i det her tilfælde vores ReportNutrientIntakePage)
-              return const ReportExercise();
-            }));
-          },
-          child: const Text("Report exercise"),
+      ],
+    ),
+    body: Column(
+      children: [
+        Container(
+          color: Colors.lightBlue,
+          margin: const EdgeInsets.fromLTRB(100, 20, 100, 20),
+          padding: const EdgeInsets.all(50),
+          height: 250,
+          width: double.infinity,
+          child: const Text("Her kommer der en graf"),
         ),
-      ),
-      const SizedBox(height: 5),
-      Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              // Bruges som "router" --> vi føres fra denne side til en anden side (i det her tilfælde vores ReportNutrientIntakePage)
-              return const TestPage();
-            }));
-          },
-          child: const Text("Test Page"),
+        const SizedBox(height: 5),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const ReportNutrientIntakePage();
+                }),
+              );
+            },
+            child: const Text("Report nutrient intake"),
+          ),
         ),
-      ),
-    ]);
-  }
+        const SizedBox(height: 5),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const ReportExercise();
+                }),
+              );
+            },
+            child: const Text("Report exercise"),
+          ),
+        ),
+        const SizedBox(height: 5),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const TestPage();
+                }),
+              );
+            },
+            child: const Text("Test Page"),
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
