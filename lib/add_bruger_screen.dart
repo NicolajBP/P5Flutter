8dccf4ac495b583a5b1e9de4217311e0aac3e16b
TheVicDK <vhbr21@student.aau.dk>
2023-10-19 12:50:41 +0200

    Tilføjelse af ny bruger Screen

---
+++ lib/add_bruger_screen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AddBrugerScreen extends StatefulWidget {
+  const AddBrugerScreen({super.key});
+
+  @override
+  State<AddBrugerScreen> createState() => _AddBrugerScreenState();
+}
+
+class _AddBrugerScreenState extends State<AddBrugerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
