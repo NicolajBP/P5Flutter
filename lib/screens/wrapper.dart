06b3a1a2d5cddc0fe259df6b5033da5c14cad84e
NicolajBP <127953052+NicolajBP@users.noreply.github.com>
2023-10-20 09:12:00 +0200

    tilføjet mapper til authentication

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+import 'home';
+
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    
+    
+    // Return enten Home eller Autenticate widget
+    return Home
+  }
+}
