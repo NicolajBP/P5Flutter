b4f635a5156b7fd9b100cd572a05ead14bb980bb
NicolajBP <127953052+NicolajBP@users.noreply.github.com>
2023-10-20 12:55:52 +0200

    Opdateret. Virker ikke endnu (fetcher ikke data)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:p5/get_user_name.dart';
 import 'package:p5/report_exercise.dart';
 import 'package:p5/report_nutrient_intake.dart';
 import 'package:p5/test_page.dart'; // Vi skal altid have den her import (giver adgang til alle widgets)
@@ -13,28 +14,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-//sign user out
-  void signUserOut() {
-    FirebaseAuth.instance.signOut(); // Laver en metode til at logge ud
+  final user = FirebaseAuth.instance.currentUser!;  // Her siger vi bare at brugeren er den nuværende bruger som er logget ind
 
-  }
+FirebaseFirestore db = FirebaseFirestore.instance;
 
-// ef3320a880efa9714fbb239e3d60a7007d0bbe20
-  final user = FirebaseAuth.instance
-      .currentUser!; 
- // Her siger vi bare at brugeren er den nuværende bruger som er logget ind
-  List<String> docIDs = [];
+
+
+ List<String> docIDs = [];
 
   // get docIDs
-  Future getDocIDs() async {
+  Future getDocID() async {
     await FirebaseFirestore.instance.collection('users').get().then(
           (snapshot) => snapshot.docs.forEach((document) {
             print(document.reference);
-            docIDs.add(document.reference
-                .id); // Vi tilføjer hvert ID til vores array af docIDs
-          }),
+            docIDs.add(document.reference.id); // Vi tilføjer hvert ID til vores array af docIDs
+          },
+          ),
         ); // Giver os  brugernes data
   }
+
+//sign user out
+  void signUserOut() {
+    FirebaseAuth.instance.signOut(); // Laver en metode til at logge ud
+  }
+
+  // @override
+  // void initState() {
+  //   getDocID();
+  //   super.initState();
+  // }
+
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +53,9 @@ class _HomePageState extends State<HomePage> {
         actions: [
           IconButton(
             // Log ud knap --> når der trykkes, logges brugeren ud
-            onPressed: signUserOut,
+            onPressed: 
+              () => FirebaseAuth.instance.signOut()
+            ,
             icon: Icon(Icons.logout),
           ),
         ],
@@ -52,7 +63,7 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Container(
-            color: Colors.lightBlue,
+            color: Colors.amber,
             margin: const EdgeInsets.fromLTRB(100, 20, 100, 20),
             padding: const EdgeInsets.all(50),
             height: 250,
@@ -98,22 +109,22 @@ class _HomePageState extends State<HomePage> {
               child: const Text("Test Page"),
             ),
           ),
-          Expanded(
-            child: FutureBuilder(
-              // Vi laver en futurebuilder fordi vi først skal vente på at dataen er blevet hentet fra databasen før vi kan lave vores ListTile
-              future: getDocIDs(),
-              builder: (context, snapshot) {
-                return ListView.builder(
-                  itemCount: docIDs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(docIDs[index]),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
+          // Expanded(
+          //   child: FutureBuilder(
+          //     // Vi laver en futurebuilder fordi vi først skal vente på at dataen er blevet hentet fra databasen før vi kan lave vores ListTile
+          //     future: getDocID(),
+          //     builder: (context, snapshot) {
+          //       return ListView.builder(
+          //         itemCount: docIDs.length,
+          //         itemBuilder: (context, index) {
+          //           return ListTile(
+          //             title: GetUserName(documentId: docIDs[index]),
+          //           );
+          //         },
+          //       );
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
