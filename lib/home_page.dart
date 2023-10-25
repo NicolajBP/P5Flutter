cc489d64b03ad2d4e589d81863723c4a137fec15
ellenritterbusch <eritte21@student.aau.dk>
2023-10-25 15:22:07 +0200

    Lille bar med tekst lavet

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,62 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:p5/report_exercise.dart';
 import 'package:p5/report_nutrient_intake.dart';
 import 'package:p5/test_page.dart'; // Vi skal altid have den her import (giver adgang til alle widgets)
+import 'package:intl/intl_browser.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+
+@override
+  State<HomePage> createState() =>
+      _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+final day = DateTime.now().weekday;
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                        //debugPrint(day.toString());
+                    }, 
+                    icon: const Icon(Icons.arrow_back),
+                    ),
+
+
+  const SizedBox(width: 10),
+
+              const Text('25.10'),
+
+
+   const SizedBox(width: 10),
+
+              IconButton(onPressed: (){
+
+              }, 
+              icon: const Icon(Icons.arrow_forward)),
+              ]), 
+            ),
+
+   // const SizedBox(height: 10),
       Container(
               color: Theme.of(context).colorScheme.primary,
               margin: const EdgeInsets.fromLTRB(50, 20, 50, 20),
-              padding: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(20),
               height: 250,
               width: double.infinity,
               child: const Text("Her kommer der en graf"),
@@ -64,6 +108,8 @@ class HomePage extends StatelessWidget {
         ],)
         )
     ],)
-    ],);
+    ],)
+      )
+    );
   }
 }
