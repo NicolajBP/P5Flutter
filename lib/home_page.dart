f637c776557175ab9545e8ee06464f3fc33604f2
NicolajBP <127953052+NicolajBP@users.noreply.github.com>
2023-10-30 13:53:42 +0100

    fikset ved fejl merge af Sebastians branch

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,13 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:p5/myEntries.dart';
-import 'package:p5/graph.dart';
 import 'package:p5/report_exercise.dart';
 import 'package:p5/report_nutrient_intake.dart';
 import 'package:p5/test_page.dart'; // Vi skal altid have den her import (giver adgang til alle widgets)
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
+import "package:p5/graph.dart";
 
 
 class HomePage extends StatefulWidget {
@@ -75,24 +75,15 @@ final user =FirebaseAuth.instance.currentUser!;
                       ),
             ),  
       Container(
-              color: Theme.of(context).colorScheme.primary,
-              margin: const EdgeInsets.fromLTRB(50, 20, 50, 20),
-              padding: const EdgeInsets.all(20),
               height: 250,
               width: double.infinity,
-              child: const Text("Her kommer der en graf"),
+              child: LiveChartWidget(),
             ),
-      const SizedBox(height: 120),
     
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
         ElevatedButton(
-    return Column(children: [
-      LiveChartWidget(),
-      const SizedBox(height: 5),
-      Center(
-        child: ElevatedButton(
           onPressed: () {
             Navigator.of(context)
                 .push(MaterialPageRoute(builder: (BuildContext context) {
@@ -140,92 +131,3 @@ final user =FirebaseAuth.instance.currentUser!;
 }
 
 // ef3320a880efa9714fbb239e3d60a7007d0bbe20
-
-
-
-          child: const Text("Report exercise"),
-        ),
-      ),
-      const SizedBox(height: 5),
-      Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              // Bruges som "router" --> vi føres fra denne side til en anden side (i det her tilfælde vores ReportNutrientIntakePage)
-              return const TestPage();
-            }));
-          },
-          child: const Text("Test Page"),
-        ),
-      ),
-    ]);
-  }
-// ef3320a880efa9714fbb239e3d60a7007d0bbe20
-}
-final user =FirebaseAuth.instance.currentUser!;
-
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Logged in as " + user.email!),
-      actions: [
-        IconButton(
-          onPressed: signUserOut,
-          icon: Icon(Icons.logout),
-        ),
-      ],
-    ),
-    body: Column(
-      children: [
-        Container(
-          height:300 ,
-          width: 300,
-          child: LiveChartWidget(),
-        ),
-    
-         SizedBox(height: 5),
-        Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return const ReportNutrientIntakePage();
-                }),
-              );
-            },
-            child: const Text("Report nutrient intake"),
-          ),
-        ),
-        const SizedBox(height: 5),
-        Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return const ReportExercise();
-                }),
-              );
-            },
-            child: const Text("Report exercise"),
-          ),
-        ),
-        const SizedBox(height: 5),
-        Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return const TestPage();
-                }),
-              );
-            },
-            child: const Text("Test Page"),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-}
