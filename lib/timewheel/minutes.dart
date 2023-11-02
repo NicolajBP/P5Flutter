a6c892a8b20ec40f38581db0cec9ed3912e6963e
TheVicDK <vhbr21@student.aau.dk>
2023-11-02 11:38:07 +0100

    Tilføjelse af minutes fil til notifikations timewh

---
+++ lib/timewheel/minutes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyMinutes extends StatefulWidget {
+  int mins;
+  MyMinutes({required this.mins});
+
+  @override
+  State<MyMinutes> createState() => _MyMinutesState();
+}
+
+class _MyMinutesState extends State<MyMinutes> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 1.0),
+      child: Center(
+        child: Text(
+          widget.mins < 10 ? '0' + widget.mins.toString() : widget.mins.toString(),
+          style: const TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
