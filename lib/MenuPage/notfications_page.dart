c2be12a5727e7556e0c8e8fe17834894571287f7
ellenritterbusch <eritte21@student.aau.dk>
2023-11-02 12:57:50 +0100

    Ny bar displayes

--- lib/MenuPage/notfications_page.dart
+++ lib/MenuPage/notfications_page.dart
@@ -10,13 +10,14 @@ class NotficationPage extends StatefulWidget {
 
 class _NotficationPageState extends State<NotficationPage> {
 bool _notifications = false;
+bool _inactivityNotificantion = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        appBar: AppBar(                                           //appbar
         title: const Text("Notifications"),
-        automaticallyImplyLeading: true, //tilbageknap i appbar
+        automaticallyImplyLeading: true,                          //tilbageknap i appbar
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
@@ -28,7 +29,7 @@ bool _notifications = false;
         children: [
       Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Material(
+        child: Material(                                        //notification on/off button
              borderRadius: BorderRadius.circular(10.0),
              elevation: 7,
              shadowColor: const Color.fromARGB(255, 209, 198, 191),
@@ -38,25 +39,51 @@ bool _notifications = false;
             borderRadius: BorderRadius.all(
             Radius.circular(20.0),),
           ),
-          child: SwitchListTile(
+          child: SwitchListTile(                                  //selve on/of button
                 title: const Text('Allow notifications'),
                 value: _notifications,
                 onChanged: (bool value) {
                     setState(() {
-                      _notifications = value;  });
-      },
+                      _notifications = value;  }); },             //hvis _notifications = true så er de slået til
       secondary: const Icon(Icons.notifications),)
           ),
         ),
       ),
 
-      _notifications == true ?
+      _notifications == true ?                                  //Når de er true displayer vi resten
         Column(
           children: [
-            Text('hello')
+                  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Material(                                        //notification on/off button
+             borderRadius: BorderRadius.circular(10.0),
+             elevation: 7,
+             shadowColor: const Color.fromARGB(255, 209, 198, 191),
+          child: Container(
+            alignment: Alignment.topCenter,
+            decoration: const BoxDecoration(       
+            borderRadius: BorderRadius.all(
+            Radius.circular(20.0),),
+          ),
+          child: SwitchListTile(                                  //selve on/of button
+                title: const Text('Allow inactivity notifications'),
+                value: _inactivityNotificantion,
+                onChanged: (bool value) {
+                    setState(() {
+                      _inactivityNotificantion = value;  }); },             //hvis _notifications = true så er de slået til
+      secondary: const Icon(Icons.notifications),)
+          ),
+        ),
+      ),
+
+      _inactivityNotificantion == true ?
+        Column(children: [],)                                             //INPUT tidswheel her
+
+        : Container()
+
           ],
         )
-        : Container()
+        : Container()                                           //Hvis ikke den er true displayer vi ikke noget
         ]
       ),  
     );  
