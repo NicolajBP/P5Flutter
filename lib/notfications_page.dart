e2e7a5b94998a048369851ae79f2fe056dc792cc
TheVicDK <vhbr21@student.aau.dk>
2023-11-02 11:38:16 +0100

    Timewheel added

--- lib/notfications_page.dart
+++ lib/notfications_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:p5/timewheel/hours.dart';
+import 'package:p5/timewheel/minutes.dart';
+import 'package:p5/timewheel/days.dart';
 class NotficationPage extends StatefulWidget {
   const NotficationPage({super.key});
 
@@ -9,6 +11,10 @@ class NotficationPage extends StatefulWidget {
 
 class _NotficationPageState extends State<NotficationPage> {
  
+
+  int currentday = 0;         //Allokerer plads til hvor værdien skal ind.
+  int currenthour = 0;
+  int currentminute = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +29,98 @@ class _NotficationPageState extends State<NotficationPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: 
-      Text('Victor laver resten XD')
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+          //dage
+          SizedBox(
+            width: 70,
+            child: ListWheelScrollView.useDelegate(
+              onSelectedItemChanged: (value) {
+                
+                setState((){
+                  currentday = value; //Værdien for dage
+                  print(value);
+                });
+              },
+              itemExtent: 50, 
+              perspective: 0.005, //Perspektivet på hjulet
+              diameterRatio: 1.3,//Diameter af hjulet
+              physics: const FixedExtentScrollPhysics(), //Sørger for den altid ender på et tal.
+              childDelegate: ListWheelChildBuilderDelegate(
+                childCount: 8, //Mængde af tal vi vil have i hjulet
+                builder: (context, index) {
+                  return MyDays(
+                    days: index,
+                  );
+                },
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            width: 10,
+          ),
+
+
+          //Timer
+          SizedBox(
+            width: 70,
+            child: ListWheelScrollView.useDelegate(
+              onSelectedItemChanged: (value) {
+                
+                setState((){
+                  currenthour = value; //Værdien for timer
+                  print(value);
+                });
+              },
+              itemExtent: 50, 
+              perspective: 0.005, //Perspektivet på hjulet
+              diameterRatio: 1.3, //Diameter af hjulet
+              physics: const FixedExtentScrollPhysics(), //Sørger for den altid ender på et tal.
+              childDelegate: ListWheelChildBuilderDelegate(
+                childCount: 24, //Mængde af tal vi vil have i hjulet
+                builder: (context, index) {
+                  return MyHours(
+                    hours: index,
+                  );
+                }
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            width: 10,
+          ),
+
+          //Minutter
+          SizedBox(
+            width: 70,
+            child: ListWheelScrollView.useDelegate(
+                onSelectedItemChanged: (value) {
+                
+                setState((){
+                  currentminute = value;  //Værdien for minut
+                  print(currentminute);
+                });
+              },
+              itemExtent: 50, 
+              perspective: 0.005,//Perspektivet på hjulet
+              diameterRatio: 1.3, //Diameter af hjulet
+              physics: const FixedExtentScrollPhysics(), //Sørger for den altid ender på et tal.
+              childDelegate: ListWheelChildBuilderDelegate(
+                childCount: 60,  //Mængde af tal vi vil have i hjulet
+                builder: (context, index) {
+                  return MyMinutes(
+                    mins: index,
+                  );
+                }
+              ),
+            ),
+          ),
+        ]
+      ), 
     );
-    
-  } 
+  }
 }
