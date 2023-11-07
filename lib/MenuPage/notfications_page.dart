62ba1bd6f0050af214ec938d1100c2e9be546b01
TheVicDK <vhbr21@student.aau.dk>
2023-11-07 11:06:46 +0100

    updates

--- lib/MenuPage/notfications_page.dart
+++ lib/MenuPage/notfications_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:p5/MenuPage/TimeWheel.dart';
 
@@ -12,6 +13,7 @@ class NotficationPage extends StatefulWidget {
 class _NotficationPageState extends State<NotficationPage> {
 bool _notifications = true;
 bool _inactivityNotificantion = true;
+int _selectedValue = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -44,45 +46,74 @@ bool _inactivityNotificantion = true;
         ),
       ),
 
-      _notifications == true ?                                  //Når de er true displayer vi resten
-        Column(
+      if (_notifications == true) Column(
           children: [
-                  Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Material(                                        //notification on/off button
-             borderRadius: BorderRadius.circular(10.0),
-             elevation: 7,
-             shadowColor: const Color.fromARGB(255, 209, 198, 191),
-          child: Container(
-            alignment: Alignment.topCenter,
-            decoration: const BoxDecoration(       
-            borderRadius: BorderRadius.all(
-            Radius.circular(20.0),),
-          ),
-          child: SwitchListTile(                                  //selve on/of button
-                title: const Text('Allow inactivity notifications'),
-                value: _inactivityNotificantion,
-                onChanged: (bool value) {
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(                                        //notification on/off button
+                borderRadius: BorderRadius.circular(10.0),
+                elevation: 7,
+                shadowColor: const Color.fromARGB(255, 209, 198, 191),
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  decoration: const BoxDecoration(       
+                    borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),),
+                  ),
+                  child: SwitchListTile(                                  //selve on/of button
+                  title: const Text('Allow inactivity notifications'),
+                  value: _inactivityNotificantion,
+                  onChanged: (bool value) {
                     setState(() {
-                      _inactivityNotificantion = value;  }); },             //hvis _notifications = true så er de slået til
-      secondary: const Icon(Icons.notifications),)
-          ),
-        ),
-      ),
+                      _inactivityNotificantion = value;  }
+                      ); 
+                      },                  //hvis _notifications = true så er de slået til
+                  secondary: const Icon(Icons.notifications),)
+                ),
+              ),
+            ),
 
-      _inactivityNotificantion == true ?
-        const Column(children: [
-          TimeWheel()
-        ],)                                             //INPUT tidswheel her
-
-        : Container()
-
+            _inactivityNotificantion == true ?
+            Scaffold(
+              body: SafeArea(
+              child: Center(
+                child: CupertinoButton.filled(
+                  child: Text('Value = $_selectedValue'), 
+                  onPressed: () => showCupertinoModalPopup(
+                    context: context, 
+                    builder: (_) => SizedBox(
+                      width: double.infinity,
+                      height: 250,
+                      child: CupertinoPicker(
+                        backgroundColor: Colors.white,
+                        itemExtent: 30,
+                        scrollController: FixedExtentScrollController(
+                          initialItem: 1,
+                        ),
+                        children: const [
+                          Text('0'),
+                          Text('1'),
+                          Text('123'),
+                        ],
+                        onSelectedItemChanged: (int value) {
+                          _selectedValue = value;
+                        }
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ):
+          ]  
+        )
+        
           ],
         )
-        : Container()                                           //Hvis ikke den er true displayer vi ikke noget
-        ]
-      ),  
-    );  
+    );                                   //Hvis ikke den er true displayer vi ikke noget
+        
+     
+      
   } 
 }
 
