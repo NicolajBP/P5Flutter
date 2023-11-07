b6fe48d4635c6814f49ca2af70c9ff5274e037c2
TheVicDK <vhbr21@student.aau.dk>
2023-11-08 00:49:21 +0100

    Anderledes hjul

--- lib/MenuPage/notfications_page.dart
+++ lib/MenuPage/notfications_page.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:p5/MenuPage/TimeWheel.dart';
+import 'package:p5/MenuPage/timewheel.dart';
 
 
 class NotficationPage extends StatefulWidget {
-  const NotficationPage({super.key});
 
+  const NotficationPage({super.key});
   @override
   State<NotficationPage> createState() => _NotficationPageState();
 }
 
 class _NotficationPageState extends State<NotficationPage> {
-bool _notifications = true;
-bool _inactivityNotificantion = true;
-int _selectedValue = 0;
+bool _notifications = false;
+bool _inactivityNotificantion = false;
+
+DateTime dateTime = DateTime(3000, 2, 1, 10, 20);
+// late int _dayTime = 0;
+// late int _hourTime = 0;
+// late int _minuteTime = 0;
+
+
+
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(                                           //appbar
         title: const Text("Notifications"),
+        automaticallyImplyLeading: true,                          //tilbageknap i appbar
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
       ),
       body: Column(
         children: [
@@ -46,74 +61,90 @@ int _selectedValue = 0;
         ),
       ),
 
+
+
+
       if (_notifications == true) Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(                                        //notification on/off button
-                borderRadius: BorderRadius.circular(10.0),
-                elevation: 7,
-                shadowColor: const Color.fromARGB(255, 209, 198, 191),
-                child: Container(
-                  alignment: Alignment.topCenter,
-                  decoration: const BoxDecoration(       
-                    borderRadius: BorderRadius.all(
-                    Radius.circular(20.0),),
-                  ),
-                  child: SwitchListTile(                                  //selve on/of button
-                  title: const Text('Allow inactivity notifications'),
-                  value: _inactivityNotificantion,
-                  onChanged: (bool value) {
+                  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Material(                                        //notification on/off button
+             borderRadius: BorderRadius.circular(10.0),
+             elevation: 7,
+             shadowColor: const Color.fromARGB(255, 209, 198, 191),
+          child: Container(
+            alignment: Alignment.topCenter,
+            decoration: const BoxDecoration(       
+            borderRadius: BorderRadius.all(
+            Radius.circular(20.0),),
+          ),
+          child: SwitchListTile(                                  //selve on/of button
+                title: const Text('Allow inactivity notifications'),
+                value: _inactivityNotificantion,
+                onChanged: (bool value) {
                     setState(() {
-                      _inactivityNotificantion = value;  }
-                      ); 
-                      },                  //hvis _notifications = true så er de slået til
-                  secondary: const Icon(Icons.notifications),)
-                ),
-              ),
-            ),
+                      _inactivityNotificantion = value;  }); },             //hvis _notifications = true så er de slået til
+      secondary: const Icon(Icons.notifications),)
+          ),
+        ),
+      ),
 
-            _inactivityNotificantion == true ?
-            Scaffold(
-              body: SafeArea(
-              child: Center(
-                child: CupertinoButton.filled(
-                  child: Text('Value = $_selectedValue'), 
-                  onPressed: () => showCupertinoModalPopup(
-                    context: context, 
-                    builder: (_) => SizedBox(
-                      width: double.infinity,
-                      height: 250,
-                      child: CupertinoPicker(
-                        backgroundColor: Colors.white,
-                        itemExtent: 30,
-                        scrollController: FixedExtentScrollController(
-                          initialItem: 1,
-                        ),
-                        children: const [
-                          Text('0'),
-                          Text('1'),
-                          Text('123'),
-                        ],
-                        onSelectedItemChanged: (int value) {
-                          _selectedValue = value;
-                        }
-                      ),
+
+
+
+      if (_inactivityNotificantion == true) 
+       Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(10.0),
+              elevation: 7,
+              shadowColor: const Color.fromARGB(255, 209, 198, 191),
+              child: Container(
+                alignment: Alignment.topCenter,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0),),
+                ),
+                child: ListTile(                                                                    //Vi laver widget med ListTile som ligner de andre
+                  leading: const Icon(Icons.access_time),                                           //Icon
+                  title: const Text('Current time between notification:'),                          //Tekst
+                  trailing: CupertinoButton(                                                        //Knap
+                    child: Text('Kl. ${dateTime.hour}:${dateTime.minute} hver ${dateTime.day}'),    //Tid der står
+                    onPressed: () {
+                      showCupertinoModalPopup(            //POPup med widget indeni
+                        context: context, 
+                        builder: (BuildContext context) => SizedBox(
+                          height: 250,
+                          width: double.infinity,
+                          child: CupertinoDatePicker(           //Vi vælger widget
+                            backgroundColor: Colors.white,      //Hvid baggrund
+                            initialDateTime: dateTime,            //Den vælger nuværende date og tid
+                            onDateTimeChanged: (DateTime newTime) {     //Når man ændrer på tid/dato skifter den NewTime variable
+                              setState(() => dateTime = newTime);
+                            },
+                            use24hFormat: true,                   //24 timers format
+                            mode: CupertinoDatePickerMode.dateAndTime,  //Her vælger man mode :)
+                            ),
+                          ),
+                          );
+                    },
+
                     ),
-                  ),
                 ),
               ),
             ),
-            ):
-          ]  
-        )
-        
+            )
+        ],
+       )
+       else Container()
+
           ],
-        )
-    );                                   //Hvis ikke den er true displayer vi ikke noget
-        
-     
-      
+        ) else Container()                                           //Hvis ikke den er true displayer vi ikke noget
+        ]
+      ),  
+    );  
   } 
 }
+
 
